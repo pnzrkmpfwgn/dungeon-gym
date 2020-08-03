34fca4ed72311b8fabfd7709a88e70ba34125e40
Ayberk Yigit <kurtknispel9@gmail.com>
2020-08-03 19:20:53 +0300

    added year to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import classes from './footer.module.css';
 const Footer = () => {
-  return <footer className={classes.test}> &#xa9; Ülkü Ayberk Yiğit</footer>;
+  const date = new Date();
+  return (
+    <footer className={classes.test}>
+      {' '}
+      &#xa9; Ülkü Ayberk Yiğit {date.getFullYear()}{' '}
+    </footer>
+  );
 };
 
 export default Footer;
